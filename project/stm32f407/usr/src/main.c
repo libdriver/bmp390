5c4d0fa721c49bb61bf2943b67a7ad7e46b1e838
e <lishifenging@outlook.com>
2023-01-24 18:22:29 +0800

    style: change the code style

--- project/stm32f407/usr/src/main.c
+++ project/stm32f407/usr/src/main.c
@@ -235,9 +235,9 @@ void bmp390_interface_fifo_receive_callback(uint8_t type)
  * @param[in] argc is arg numbers
  * @param[in] **argv is the arg address
  * @return    status code
- *             - 0 success
- *             - 1 run failed
- *             - 5 param is invalid
+ *            - 0 success
+ *            - 1 run failed
+ *            - 5 param is invalid
  * @note      none
  */
 uint8_t bmp390(uint8_t argc, char **argv)
